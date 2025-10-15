1c49c30abc72fb2a30c41b4afed3d411d913fcb5
corbin Boughen <50217007+theBough@users.noreply.github.com>
2025-10-15 11:33:35 -0500

    Update player.js

--- player.js
+++ player.js
@@ -6,6 +6,15 @@ function Player(x,y,w,h,col,img){
   this.col = col;
   this.img = loadImage(img);
   
+  //this function will move the player
+  this.update = function(){
+    if(keyIsDown(37)){
+      //this is the left arrow
+      this.x -= 5;
+    }
+  }//end update
+  
+  //this function displays the image onto the canvas
   this.display = function(){
     //fill(this.col);
     //rect(this.x, this.y, this.w, this.h);
