cf35be15f3ffef0b3c35681cd8db47a8efb42d7b
corbin Boughen <50217007+theBough@users.noreply.github.com>
2025-10-15 11:51:18 -0500

    Add additional parameters to Player constructor

--- sketch.js
+++ sketch.js
@@ -2,7 +2,7 @@ let p;
 
 function setup() {
   createCanvas(400, 400);
-   p =new Player(200,200,20,20,"red","player.png")
+   p =new Player(200,200,20,20,"red","player.png",37,39,38,40,1)
 }
 
 function draw() {
