9a11cb009d77c8d8b51f6712274cb95b255a2154
corbin Boughen <50217007+theBough@users.noreply.github.com>
2025-10-16 11:52:39 -0500

    Refactor Player class with animation and update methods

--- player.js
+++ player.js
@@ -1,4 +1,4 @@
-function Player(x,y,w,h,col,img,l,r,u,d,speed){
+function Player(x, y, w, h, col, img, l, r, u, d, speed) {
   this.x = x;
   this.y = y;
   this.w = w;
@@ -9,22 +9,37 @@ function Player(x,y,w,h,col,img,l,r,u,d,speed){
   this.r = r;
   this.u = u;
   this.d = d;
-  this.speed = speed
+  this.speed = speed;
+  this.walkLeft = [
+    loadImage("ltOne.png"),
+    loadImage("ltTwo.png"),
+    loadImage("ltThree.png")    
+  ]
+  this.walkCycle = 0;
+  let stop;
   
+  this.animateLeft = function(){
+    this.img = this.walkLeft[this.walkCycle];
+    this.walkCycle += 1;
+    //the number 2 is because I have a 3 picture walk cycle
+    if(this.walkCycle>2){
+      this.walkCycle = 0;
+    }
+  } 
   //this function will move the player
-  this.update = function(){
-    if(keyIsDown(this.l)){
+  this.update = function () {
+    if (keyIsDown(this.l)) {
       //this is the left arrow
       this.x -= this.speed;
+      
     }
-  }//end update
-  
+  }; //end update
+
   //this function displays the image onto the canvas
-  this.display = function(){
+  this.display = function () {
     //fill(this.col);
     //rect(this.x, this.y, this.w, this.h);
-    this.img.resize(this.w, this.h)
-    image(this.img,this.x,this.y)
-  }//end display
-  
-}//end Player
+    this.img.resize(this.w, this.h);
+    image(this.img, this.x, this.y);
+  }; //end display
+} //end Player
