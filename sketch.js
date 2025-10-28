7f7319ea42e35ecf2146e5ca0bacbe2ea0ae07c1
corbin Boughen <50217007+theBough@users.noreply.github.com>
2025-10-28 11:13:31 -0500

    Add array for objects and update draw function
    Initialized an array for storing objects and updated the draw function to display them.

--- sketch.js
+++ sketch.js
@@ -1,14 +1,20 @@
 let p;
+let w = [];
 
 function setup() {
   createCanvas(400, 400);
   p = new Player(200, 200, 20, 20, "red", "player.png", 37, 39, 38, 40, 1);
+  roomOne();
 }
 
 function draw() {
   background(220);
   p.display();
   p.update();
+  for(i=0 ; i < w.length ; i++){
+    w[i].display()
+  }//end loop
+ 
 }
 
 
