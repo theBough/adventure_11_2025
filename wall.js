ced07254b0bde8c9c1f3d112bbcbfab4607057f9
corbin Boughen <50217007+theBough@users.noreply.github.com>
2025-10-28 11:31:03 -0500

    Fix syntax error in roomOne function

--- wall.js
+++ wall.js
@@ -14,5 +14,7 @@ function Wall(x,y,w,h,col){
 function roomOne(){
   w=[];
   w.push(new Wall(0,225,400,20,"#4CAF50"))
+  w.push(new Wall(0,0,200,200,"#4CAF50"))
+  w.push(new Wall(380,0,20,200,"#4CAF50"))
   
 }
