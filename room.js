c2a9a7ec0e973d47634ccae3c3b2827476b5d5cd
corbin Boughen <50217007+theBough@users.noreply.github.com>
2025-10-30 11:46:15 -0500

    Add loadRooms function to initialize room data
    Added a function to load rooms and initialized room data.

--- room.js
+++ room.js
@@ -1,7 +1,15 @@
+function loadRooms(){
+  rooms = [
+    [roomOne, roomTwo]
+  ]
+}
 function roomOne(){
   w=[];
   w.push(new Wall(0,225,400,20,"#4CAF50"))
   w.push(new Wall(0,0,200,200,"#4CAF50"))
-  w.push(new Wall(380,0,20,200,"#4CAF50"))
-  
+  w.push(new Wall(380,0,20,200,"#4CAF50")) 
+}
+function roomTwo(){
+  w=[];
+  w.push(new Wall(0,225,400,20,"#4CAF90"))
 }
