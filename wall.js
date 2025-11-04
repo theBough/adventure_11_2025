edf9e21e70a1d2a2df1f1d7224e8cf1ef480a0c6
corbin Boughen <50217007+theBough@users.noreply.github.com>
2025-11-04 11:05:37 -0600

    Update wall.js

--- wall.js
+++ wall.js
@@ -26,4 +26,30 @@ function roomChange(){
   }
 }//end roomChange
 
+function wallCollision() {
+  for (var i = 0; i < w.length; i++) {
+
+    //check if we hit the left of any wall
+    if (p.y <= w[i].y + w[i].h && p.y + p.h >= w[i].y && p.x <= w[i].x + w[i].w && p.x >= w[i].x) {
+      p.x += p.speed
+    }
+
+    //check if we hit the right of any wall
+    if (p.y <= w[i].y + w[i].h && p.y + p.h >= w[i].y && p.x + p.w >= w[i].x && p.x <= w[i].x + w[i].w) {
+     p.x -= p.speed
+    }
+
+
+    if (p.x <= w[i].x + w[i].w && p.x + p.w >= w[i].x && p.y <= w[i].y + w[i].h && p.y >= w[i].y) {
+      p.y += p.speed
+    }
+
+    //check if we hit the top of any wall
+    if (p.x <= w[i].x + w[i].w && p.x + p.w >= w[i].x && p.y + p.h >= w[i].y && p.y <= w[i].y + w[i].h) {
+     p.y -= p.speed
+    }
+
+  }//end loop
+}//end colision
+
 
